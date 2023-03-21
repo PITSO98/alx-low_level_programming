ff111273969c4e968c51e16bdd7f1f77bb9f8b6f
PITSO98 <sqinisekomrbe@gmail.com>
2023-03-20 21:00:19 -0700

    r

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef _object_like_macro_h_
+#define _object_like_macro_h_
+
+#define SIZE 1024
+
+#endif /* _object_like_macro_h_ */
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+
