bd1974f2e3806b5a214ade61b31282554f11476b
PITSO98 <sqinisekomrbe@gmail.com>
2023-04-18 05:35:41 -0700

    task6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists"
+
+/**
+ * pop_listint - Delete the first head node of  linked list.
+ * @head: Pointer to a list.
+ * Return: Integer if success.
+ * or 0 if list is empty
+ **/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = *head;
+	*head = temp->next;
+	num = temp->n;
+	free(temp);
+	return (num);
+}
